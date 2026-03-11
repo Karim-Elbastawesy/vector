17d3abdb809c1bb51b7efdbc84aa6577539f5555
Karim-Elbastawesy <karimelbastawesy20@gmail.com>
2026-03-11 23:45:30 +0200

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-purple-400 flex justify-center min-h-screen items-center font-bold text-3xl">Hello, user! </h1>
+    </div>
+  )
+}
