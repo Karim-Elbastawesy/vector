4c5ee90b6f2be619db5903f4a2e8077ce4e0cca4
Karim-Elbastawesy <karimelbastawesy20@gmail.com>
2026-04-05 20:36:16 +0200

    rename fix

--- src/app/_components/ProductItem/actions/addCart.action.ts
+++ src/app/_components/ProductItem/actions/addCart.action.ts
@@ -1,22 +1,18 @@
 import { getToken } from "@/lib/auth";
-import { getTokenFn } from "@/utilities/getTokenFun";
+import { getAuthToken } from "@/utilities/getTokenFun";
 import { cookies } from "next/headers";
 import { json } from "zod";
 
 export async function addToCart(productId: string) {
-  
-    const token = await getTokenFn()
-    
+  const token = await getAuthToken();
 
-    if (!token) {
-        throw new Error("Unauthorized")
-
-    }
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
 
   const data = await fetch(`${process.env.API}cart`, {
     method: "post",
     body: JSON.stringify({ productId }),
     headers: { token, "Content-type": "application/json" },
   });
-    
 }
